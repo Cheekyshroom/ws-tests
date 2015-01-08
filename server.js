bf6b99905f6c936d5e91c2d558b5594bd8e50069
Cole Faust <colecfaust@gmail.com>
2015-01-08 11:18:46 -0800

    fixed logging in (duplicate ids)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname+'/files'));
 app.listen(3000, function(){
-    
+
 });
 
 var WebSocketServer = require('ws').Server;
@@ -10,7 +10,7 @@ var wss = new WebSocketServer({port:8080});
 
 wss.broadcast = function(data){
     wss.clients.forEach(function(client){
-	client.send(data);
+        client.send(data);
     });
 };
 
@@ -27,16 +27,19 @@ userlist['timmy'] = "foobar";
 wss.on('connection', function (ws){
     console.log("user "+usercount+" connected");
     ws.on('message', function (message){
-	var data = JSON.parse(message);
-	if (data.type === "login"){
-	    if (passwordMatches(userlist, data.username, data.password))
-	    {
-		ws.loggedin = true;
-	    }
-	} else if (ws.loggedin) {
-	    console.log('got %s', message);
-	    wss.broadcast(ws.username+": "+message);
-	}
+        var data = JSON.parse(message);
+        if (data.type === "login"){
+            console.log("user attempting to log in");
+            if (passwordMatches(userlist, data.username, data.password))
+            {
+                ws.loggedin = true;
+                ws.username = data.username;
+                console.log(data.username+" logged in!");
+            }
+        } else if (ws.loggedin && data.type === "chat") {
+            console.log('got %s', message);
+            wss.broadcast(ws.username+": "+data.message);
+        }
     });
     ws.username = "User"+usercount++;
     ws.send("Hello");
